f40505c96337f9c263ac413f4fcbaf04ef73fce3
tomoyuki-tanaka <masuminium5@gmail.com>
2017-04-07 23:10:38 +0900

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+require 'faker'
+
+Post.destroy_all
+User.destroy_all
+
+Faker::Config.locale = :ja
+
+User.create!(
+  name: Faker::Name.name,
+  email: Faker::Internet.safe_email,
+  password: 'test123',
+  password_confirmation: 'test123'
+)
+
+10.times do
+  Post.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
+end
